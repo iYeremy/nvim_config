2632114a24500c387f5acbafe3ce03132f9fb811
iyeremy <yeremyvegap@gmail.com>
2026-03-01 18:22:51 -0500

    feat(buffers): add delete others and buffer picker keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,4 +22,25 @@ vim.keymap.set("n", "<leader>bd", ":bd<CR>", {
   desc = "Buffer: Delete",
 })
 
+-- Delete all buffers except the current onee
+vim.keymap.set("n", "<leader>bo", function()
+  local current = vim.api.nvim_get_current_buf()
+
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if buf ~= current and vim.bo[buf].buflisted then
+      vim.api.nvim_buf_delete(buf, {})
+    end
+  end
+end, {
+  silent = true,
+  desc = "Buffer: Delete Others",
+})
+
+-- Open buffer picker (Snacks)
+vim.keymap.set("n", "<leader>bb", function()
+  Snacks.picker.buffers()
+end, {
+  silent = true,
+  desc = "Buffer: List",
+})
 
