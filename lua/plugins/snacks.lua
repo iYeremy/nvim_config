5419750215f30ab9d4e4687b00bb1aa9c2f0a856
iyeremy <yeremyvegap@gmail.com>
2026-03-01 18:06:37 -0500

    feat: implement Zen Mode and refacture kb for Dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -26,8 +26,9 @@ return {
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
     { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
-    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
-    -- Atajo extra para volver al Dashboard si quieres
-    { "<leader>bdd", function() Snacks.dashboard.open() end, desc = "Open Dashboard" },
+    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" }, 
+	{ "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+	{ "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+    { "<leader>b-", function() Snacks.dashboard.open() end, desc = "Open Dashboard" },
   }
 }
