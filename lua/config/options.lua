89561d6514ac1dfc617ae89269f3af4cd61cf86e
iyeremy <yeremyvegap@gmail.com>
2026-03-01 21:00:35 -0500

    style(diagnostics): apply nerd font symbols to improve diagnostic visibility

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -24,3 +24,19 @@ opt.swapfile = false          -- Disable swap files
 vim.diagnostic.config({
   virtual_text = true,
 })
+
+-- =============================================================================
+-- GLOBAL DIAGNOSTIC SIGNS
+-- =============================================================================
+
+local signs = { 
+  Error = " ", -- Un círculo con una X
+  Warn  = " ", -- Un triángulo de advertencia
+  Hint  = "󰠠 ", -- Una bombilla o foco
+  Info  = " "  -- Un círculo de información
+}
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
