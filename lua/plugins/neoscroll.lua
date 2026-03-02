619ec11a20fad4a64696c4401093d7dc01e89b7d
iyeremy <yeremyvegap@gmail.com>
2026-03-02 07:46:50 -0500

    style(scrolling): configure quadratic easing for more natural movement

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,4 +1,23 @@
+-- =============================================================================
+-- SMOOTH SCROLLING (neoscroll.nvim)
+-- Description: Adds butter-smooth scrolling to Neovim for better navigation.
+-- =============================================================================
+
 return {
   "karb94/neoscroll.nvim",
-  opts = {},
+  -- Performance: Only load when we actually start scrolling
+  keys = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+  opts = {
+    -- Time in milliseconds to complete the scroll
+    duration_multiplier = 1.0, 
+    -- Easing function: "quadratic", "cubic", "quartic", "quintic", "circular" or "sine"
+    easing = "quadratic",      
+    -- Stop at the end of the file
+    stop_eof = true,           
+    -- Hide cursor during scroll
+    hide_cursor = true,        
+  },
+  config = function(_, opts)
+    require("neoscroll").setup(opts)
+  end,
 }
